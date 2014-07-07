53b32fc9706ee8653f1855000dee2361490de4f4
Barry vd. Heuvel <barryvdh@gmail.com>
2014-07-07 08:56:17 +0200

    Can also return nothing

--- src/ValidatingTrait.php
+++ src/ValidatingTrait.php
@@ -136,7 +136,7 @@ trait ValidatingTrait {
      * an empty array
      *
      * @param  string $ruleset
-     * @return array
+     * @return array|void
      */
     public function getDefaultRules($ruleset = null)
     {
